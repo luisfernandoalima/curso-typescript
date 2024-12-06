24619a51ac54dafc31223a1cc3e491857ad98735
Luis Lima <luisfernando.lima2005@gmail.com>
2024-12-06 16:50:14 -0300

    Criação de objetos e renomeando pastas

---
+++ ex004 - Objetos/dist/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let objeto;
+let objeto2;
+let objeto3;
+objeto3 = { nome: "Luis", idade: 19, legal: true };
+console.log(objeto3.nome);
---
+++ ex004 - Objetos/main.ts
@@ -0,0 +1,13 @@
+let objeto: object;
+
+let objeto2: Record<string, string>;
+
+let objeto3: {
+  nome: string;
+  idade: number;
+  legal: boolean;
+};
+
+objeto3 = { nome: "Luis", idade: 19, legal: true };
+
+console.log(objeto3.nome);

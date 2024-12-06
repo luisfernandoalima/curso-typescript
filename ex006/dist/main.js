422d9b474da4f9c8db18391317548687e490a509
Luis Lima <luisfernando.lima2005@gmail.com>
2024-12-06 17:15:12 -0300

    Tipando funções

---
+++ ex006/dist/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let a = 5;
+let b = 10;
+function soma(a, b) {
+    return a + b;
+}
+const mult = (a, b) => a * b;
+console.log(soma(a, b));
+console.log(mult(a, b));
---
+++ ex006/main.ts
@@ -0,0 +1,12 @@
+let a: number = 5
+let b: number = 10
+
+function soma(a: number, b: number): number{
+    return a + b
+}
+
+const mult = (a: number, b: number): number => a*b
+
+console.log(soma(a,b))
+
+console.log(mult(a,b))
